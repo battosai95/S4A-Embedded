65ff78767c0b17f8c7797c45f4b69bb42311e5aa
root <tristanbouju@gmail.com>
2016-02-08 17:51:07 +0100

    update files from web_service branch modification

--- ScratchInoConvertor/src/AutoDetectSerial.py
+++ ScratchInoConvertor/src/AutoDetectSerial.py
@@ -7,23 +7,26 @@ import sys
 import serial.tools.list_ports
 
 
+class AutoDetectSerialError(Exception):
+    pass
+
+
 class AutoDetectSerial(object):
 
     def __init__(self):
         pass
 
-    def __getUsbDevicesLinux(self):
+    def __get_usb_devices_linux(self):
         '''
         get usb devices information with a shell script
         :return: [ "usb device information" ]
         '''
         proc = subprocess.Popen('./getUsbDevices.sh', stdout=subprocess.PIPE)
-        usbDeviceTmp = proc.stdout.read()
-        usbDevice = usbDeviceTmp.split("\n")
+        usb_device_tmp = proc.stdout.read()
+        usb_device = usb_device_tmp.split("\n")
+        return usb_device
 
-        return usbDevice
-
-    def __getArduinosPathLinux(self):
+    def __get_arduinos_path_linux(self):
 
         '''
         get arduinos information on linux system
@@ -34,15 +37,16 @@ class AutoDetectSerial(object):
         :return: [ ( "arduino path", "arduino description") ]
         '''
 
-        usbDevices = self.__getUsbDevicesLinux()
-        arduinoDevices = []
-        for usbDevice in usbDevices:
-            usbArduinoDevicesFound = re.findall('(\/dev\/.*) -(.*Arduino.*)', usbDevice)
-            if usbArduinoDevicesFound != []:
-                arduinoDevices.append(usbArduinoDevicesFound[0])
-        return arduinoDevices
+        usb_devices = self.__get_usb_devices_linux()
+        arduino_devices = []
+        for usb_device in usb_devices:
+            usb_arduino_devices_found = re.findall(
+                '(\/dev\/.*) -(.*Arduino.*)', usb_device)
+            if usb_arduino_devices_found != []:
+                arduino_devices.append(usb_arduino_devices_found[0])
+        return arduino_devices
 
-    def __getArduinoPathWindows(self):
+    def __get_arduino_path_windows(self):
 
         '''
         get arduinos information on windows system
@@ -50,32 +54,35 @@ class AutoDetectSerial(object):
         '''
         devices = list(serial.tools.list_ports.comports())
 
-        arduinoDevices = []
+        arduino_devices = []
 
         for device in devices:
             if device[1].startswith("Arduino"):
-                arduinoDevices.append((device[0], device[1]))
-        return arduinoDevices
+                arduino_devices.append((device[0], device[1]))
+        return arduino_devices
 
-
-    def getArduinosPath(self):
+    def get_arduinos_path(self):
         '''
         get arduinos information, description content depends on OS
         :return: [ ( "arduino path", "arduino description") ]
         '''
+        arduino_paths = []
         if sys.platform.startswith('linux'):
-            print "Linux"
-            return self.__getArduinosPathLinux()
+            print "_linux"
+            arduino_paths = self.__get_arduinos_path_linux()
 
         elif sys.platform.startswith('win'):
-            print "Windows"
-            return self.__getArduinoPathWindows()
+            print "_windows"
+            arduino_paths = self.__get_arduino_path_windows()
+
+        if arduino_paths == []:
+            raise AutoDetectSerialError("Error: no Arduino board is connected")
+
+        return arduino_paths
 
 
 if __name__ == '__main__':
-    autoDetectSerial = AutoDetectSerial()
-    print autoDetectSerial.getArduinosPath()
-
-    arduinoSerial = autoDetectSerial.getArduinosPath()
-    arduinoSerial = arduinoSerial[0]
-    print arduinoSerial[0], arduinoSerial[1]
+    auto_detect_serial = AutoDetectSerial()
+    arduino_serial = auto_detect_serial.get_arduinos_path()
+    arduino_serial = arduino_serial[0]
+    print arduino_serial[0], arduino_serial[1]
